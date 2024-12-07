75a4bb0179f7e0d37b6678c1b2371cf657e3378b
Yves Brissaud <yves.brissaud@gmail.com>
2024-12-07 10:31:36 +0100

    day 07
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- d07/main.go
+++ d07/main.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/eunomie/aoc2024/inputs"
+	"github.com/eunomie/aoc2024/sugar"
 )
 
 func main() {
-	fmt.Println(d07p1(inputs.D07))
-	fmt.Println(d07p2(inputs.D07))
+	fmt.Println(d07(inputs.D07, false))
+	fmt.Println(d07(inputs.D07, true))
 }
 
-func d07p1(input string) int {
-	panic("not implemented")
+func d07(input string, concat bool) int {
+	var testValueStr, numbersStr string
+	var ok bool
+	var testValue int
+	var numbers []int
+	var res int
+
+	for _, line := range inputs.Lines(input) {
+		testValueStr, numbersStr, ok = strings.Cut(line, ": ")
+		if !ok {
+			panic("parse error")
+		}
+		testValue = sugar.MustAtoi(testValueStr)
+
+		numbers = inputs.Numbers(numbersStr, " ")
+
+		if isCalibrated(testValue, numbers[0], numbers[1:], concat) {
+			res += testValue
+		}
+	}
+
+	return res
 }
 
-func d07p2(input string) int {
-	panic("not implemented")
+func isCalibrated(testValue, n int, numbers []int, concat bool) bool {
+	if len(numbers) == 0 {
+		return testValue == n
+	}
+	if n > testValue {
+		return false
+	}
+	return isCalibrated(testValue, n+numbers[0], numbers[1:], concat) ||
+		isCalibrated(testValue, n*numbers[0], numbers[1:], concat) ||
+		concat && isCalibrated(testValue, concatNums(n, numbers[0]), numbers[1:], concat)
+}
+
+func concatNums(a, b int) int {
+	return sugar.MustAtoi(fmt.Sprintf("%d%d", a, b))
 }
--- d07/main_test.go
+++ d07/main_test.go
@@ -3,21 +3,37 @@ package main
 import "testing"
 
 func TestD07P1(t *testing.T) {
-	input := ``
+	input := `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
 
-	expected := 0
+	expected := 3749
 
-	if v := d07p1(input); v != expected {
+	if v := d07(input, false); v != expected {
 		t.Errorf("expcted %v, got %v", expected, v)
 	}
 }
 
 func TestD07P2(t *testing.T) {
-	input := ``
+	input := `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
 
-	expected := 0
+	expected := 11387
 
-	if v := d07p2(input); v != expected {
+	if v := d07(input, true); v != expected {
 		t.Errorf("expcted %v, got %v", expected, v)
 	}
 }
