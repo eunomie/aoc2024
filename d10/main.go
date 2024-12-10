104842f4ce87b2f0de1afc37d929a8166b0c3eb1
Yves Brissaud <yves.brissaud@gmail.com>
2024-12-10 07:31:14 +0100

    day 10
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- d10/main.go
+++ d10/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/eunomie/aoc2024/inputs"
+	"github.com/eunomie/aoc2024/sugar"
 )
 
 func main() {
@@ -11,9 +13,72 @@ func main() {
 }
 
 func d10p1(input string) int {
-	panic("not implemented")
+	board := sugar.NewBoard(input)
+
+	var scores int
+
+	board.ForEachRunes(func(r sugar.Rune) {
+		m := map[int]struct{}{}
+		ts := find(board, r, '1')
+		for _, s := range ts {
+			m[s] = struct{}{}
+		}
+		scores += len(m)
+	}, '0')
+
+	return scores
+}
+
+func find(board *sugar.Board, r sugar.Rune, next rune) (scores []int) {
+	var n sugar.Rune
+	n = r.Top()
+	if !n.IsEmpty() && n.Rune() == next {
+		if next == '9' {
+			scores = append(scores, n.Index())
+		} else {
+			s := find(board, n, next+1)
+			scores = append(scores, s...)
+		}
+	}
+	n = r.Right()
+	if !n.IsEmpty() && n.Rune() == next {
+		if next == '9' {
+			scores = append(scores, n.Index())
+		} else {
+			s := find(board, n, next+1)
+			scores = append(scores, s...)
+		}
+	}
+	n = r.Bottom()
+	if !n.IsEmpty() && n.Rune() == next {
+		if next == '9' {
+			scores = append(scores, n.Index())
+		} else {
+			s := find(board, n, next+1)
+			scores = append(scores, s...)
+		}
+	}
+	n = r.Left()
+	if !n.IsEmpty() && n.Rune() == next {
+		if next == '9' {
+			scores = append(scores, n.Index())
+		} else {
+			s := find(board, n, next+1)
+			scores = append(scores, s...)
+		}
+	}
+	return scores
 }
 
 func d10p2(input string) int {
-	panic("not implemented")
+	board := sugar.NewBoard(input)
+
+	var scores int
+
+	board.ForEachRunes(func(r sugar.Rune) {
+		ts := find(board, r, '1')
+		scores += len(ts)
+	}, '0')
+
+	return scores
 }
--- d10/main_test.go
+++ d10/main_test.go
@@ -3,9 +3,16 @@ package main
 import "testing"
 
 func TestD10P1(t *testing.T) {
-	input := ``
+	input := `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`
 
-	expected := 0
+	expected := 36
 
 	if v := d10p1(input); v != expected {
 		t.Errorf("expcted %v, got %v", expected, v)
@@ -13,9 +20,16 @@ func TestD10P1(t *testing.T) {
 }
 
 func TestD10P2(t *testing.T) {
-	input := ``
+	input := `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`
 
-	expected := 0
+	expected := 81
 
 	if v := d10p2(input); v != expected {
 		t.Errorf("expcted %v, got %v", expected, v)
